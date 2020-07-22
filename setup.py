f5b3a0c1c9aa5a0ecf0ec72a5011676367c2b6f8
Valentin Louf <valentin.louf@bom.gov.au>
2020-07-22 13:58:36 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'arm_pyart', 'pandas', 'netCDF4', 'dask', 'crayons'],  # Optional
+    install_requires=['numpy', 'arm_pyart', 'pandas', 'netCDF4', 'dask'],  # Optional
 
     # List additional URLs that are relevant to your project as a dict.
     #
