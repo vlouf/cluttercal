fddfb4c567e0b8841694239a3672f46bb01b42ee
Valentin Louf <valentin.louf@bom.gov.au>
2021-02-11 22:15:29 +1100

    MODIF: new cluttermask

--- cluttercal/cluttermask.py
+++ cluttercal/cluttermask.py
@@ -1,5 +1,5 @@
 """
-Generate clutter mask using Py-ART.
+Generate clutter mask
 
 title: cluttermask.py
 author: Valentin Louf
@@ -10,64 +10,71 @@ date: 11/02/2021
 import os
 import warnings
 import traceback
-from typing import Tuple, List
+
+from typing import Tuple, List, Dict
 
 import pyart
+import pyodim
 import numpy as np
 import xarray as xr
 import dask.bag as db
 
 
-def buffer(func):
+def read_radar(infile: str, refl_name: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
-    Decorator to catch and kill error message. Almost want to name the function
-    dont_fail.
-    """
-
-    def wrapper(*args, **kwargs):
-        try:
-            rslt = func(*args, **kwargs)
-        except Exception:
-            traceback.print_exc()
-            rslt = None
-        return rslt
-
-    return wrapper
-
-
-@buffer
-def _read_radar(infile, refl_name):
-    """
-    Read input radar file
+    Read radar data using pyodim or pyart depending on the radar format.
 
     Parameters:
     ===========
-    radar_file_list: str
-        List of radar files.
+    infile: str
+        Input file
     refl_name: str
         Uncorrected reflectivity field name.
 
     Returns:
     ========
-    radar: PyART.Radar
-        Radar data.
+    radar: xr.Dataset
+        Radar dataset, first elevation only.
     """
-    try:
-        if infile.lower().endswith((".h5", ".hdf", ".hdf5")):
+    use_pyodim = False
+    if infile.lower().endswith((".h5", ".hdf", ".hdf5")):
+        try:
+            r = pyodim.read_odim(infile)
+            radar = r[0].compute()
+            use_pyodim = True
+        except Exception:
             radar = pyart.aux_io.read_odim_h5(infile, include_fields=[refl_name])
-        else:
-            radar = pyart.io.read(infile, include_fields=[refl_name])
-    except Exception:
-        print(f"Reading error of {infile}.")
-        raise
+    else:
+        radar = pyart.io.read(infile, include_fields=[refl_name])
 
     try:
-        radar.fields[refl_name]
+        _ = radar[refl_name].values
     except KeyError:
-        del radar
         raise KeyError(f"Problem with {os.path.basename(infile)}: uncorrected reflectivity not present.")
 
-    return radar
+    if use_pyodim:
+        r = radar.range.values
+        azi = np.round(radar.azimuth.values % 360).astype(int)
+        refl = radar[refl_name].values
+        try:
+            refl = refl.filled(np.NaN)
+        except Exception:
+            pass
+    else:
+        elev = radar.elevation["data"]
+        lowest_tilt = np.argmin([elev[i][0] for i in radar.iter_slice()])
+        sl = radar.get_slice(lowest_tilt)
+
+        r = radar.range["data"]
+        azi = np.round(radar.azimuth["data"][sl] % 360).astype(int)
+        refl = radar.fields[refl_name]["data"][sl].filled(np.NaN)
+
+    return r, azi, refl
+
+
+def get_metadata(infile: str) -> Dict:
+    attrs = {}
+    return attrs
 
 
 def find_clutter_pos(
@@ -80,6 +87,12 @@ def find_clutter_pos(
     ==========
     infile: str
         Input radar file
+    refl_name: str
+        Uncorrected reflectivity field name.
+    refl_threshold: float
+        Minimum reflectivity value threshold.
+    max_range: int
+        Maximum range (in m) threshold.
 
     Returns:
     ========
@@ -90,27 +103,22 @@ def find_clutter_pos(
     zclutter: ndarray
         Reflectivity value of clutter pixels.
     """
-    radar = _read_radar(infile, refl_name)
-    if radar is None:
+    try:
+        r, azi, refl = read_radar(infile, refl_name)
+    except Exception:
+        traceback.print_exc()
         return None
-    elev = radar.elevation["data"]
-    lowest_tilt = np.argmin([elev[i][0] for i in radar.iter_slice()])
-    sl = radar.get_slice(lowest_tilt)
 
-    r = radar.range["data"]
-    azi = np.round(radar.azimuth["data"][sl] % 360).astype(int)
-    refl = radar.fields[refl_name]["data"][sl].filled(np.NaN)
     R, A = np.meshgrid(r, azi)
 
     with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
+        warnings.simplefilter('ignore')
         pos = (R < max_range) & (refl > refl_threshold)
 
     rclutter = 1000 * (R[pos] / 1e3).astype(int)
     aziclutter = A[pos]
     zclutter = np.round(2 * refl[pos]) / 2
 
-    del radar
     return rclutter, aziclutter, zclutter
 
 
@@ -156,7 +164,7 @@ def clutter_mask(
         bag = db.from_sequence(argslist).starmap(find_clutter_pos)
         rslt = bag.compute()
     else:
-        rslt = [find_clutter_pos(f, refl_name, refl_threshold, max_range) for f in radar_file_list]
+        rslt = [find_clutter_pos(d) for d in radar_file_list]
 
     rslt = [r for r in rslt if r is not None]
     if len(rslt) == 0:
@@ -176,9 +184,11 @@ def clutter_mask(
         zmask[idx, apos, rpos] = refl
     zmask = np.ma.masked_invalid(zmask)
 
-    arr = (~np.ma.masked_less(freq_ratio * cmask.sum(axis=0), freq_threshold).mask) & (
+    arr = np.zeros((na, nr), dtype=np.int16)
+    pos = (~np.ma.masked_less(freq_ratio * cmask.sum(axis=0), freq_threshold).mask) & (
         zmask.mean(axis=0) > refl_threshold
     )
+    arr[pos] = 1
 
     if np.sum(arr) == 0:
         print("No Clutter detected. Not creating clutter mask.")
@@ -186,14 +196,13 @@ def clutter_mask(
 
     dset = xr.Dataset(
         {
-            "clutter_mask": (("azimuth", "range"), arr),
+            "clutter_mask": (("azimuth", "range"), arr.astype(np.int16)),
             "azimuth": (("azimuth"), np.arange(na).astype(np.int16)),
             "range": (("range"), np.arange(nr).astype(np.int16)),
         }
     )
 
-    radar = _read_radar(radar_file_list[0], refl_name)
-    dset.attrs = radar.metadata
+    dset.attrs = get_metadata(radar_file_list[0])
     dset.attrs["range_max"] = max_range
     dset.range.attrs = {"units": "km", "long_name": "radar_range"}
     dset.azimuth.attrs = {"units": "degrees", "long_name": "radar_azimuth"}
@@ -203,7 +212,6 @@ def clutter_mask(
         "description": "Clutter position in a coarse polar grid.",
     }
 
-    del radar
     if output is not None:
         dset.to_netcdf(output)
         print(f"Clutter mask {output} created.")
